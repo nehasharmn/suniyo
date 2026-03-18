207455bce7f05d7120237056c5d395d351c3c68f
Pradeep Sharma <psharma@chdinvestments.net>
2026-03-18 19:00:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import usePageTracking from './hooks/usePageTracking';
 
 export default function Layout({ children }) {
   return (
