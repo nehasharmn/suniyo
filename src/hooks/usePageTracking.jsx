e37fc396138fdc43d7213494e77812d1dfb758ba
Pradeep Sharma <psharma@chdinvestments.net>
2026-03-18 19:13:04 +0000

    File changes

--- src/hooks/usePageTracking.jsx
+++ src/hooks/usePageTracking.jsx
@@ -9,6 +9,25 @@ function getDevice() {
   return 'desktop';
 }
 
+function getDeviceName() {
+  const ua = navigator.userAgent;
+  let os = 'Unknown OS';
+  if (/windows/i.test(ua)) os = 'Windows';
+  else if (/mac os/i.test(ua)) os = 'macOS';
+  else if (/iphone|ipad|ipod/i.test(ua)) os = 'iOS';
+  else if (/android/i.test(ua)) os = 'Android';
+  else if (/linux/i.test(ua)) os = 'Linux';
+
+  let browser = 'Unknown Browser';
+  if (/chrome/i.test(ua) && !/edg/i.test(ua)) browser = 'Chrome';
+  else if (/firefox/i.test(ua)) browser = 'Firefox';
+  else if (/safari/i.test(ua) && !/chrome/i.test(ua)) browser = 'Safari';
+  else if (/edg/i.test(ua)) browser = 'Edge';
+  else if (/opr|opera/i.test(ua)) browser = 'Opera';
+
+  return `${os} / ${browser}`;
+}
+
 export default function usePageTracking() {
   const location = useLocation();
 
@@ -21,12 +40,25 @@ export default function usePageTracking() {
           if (user?.email) visitor_email = user.email;
         } catch (_) {}
 
+        let ip_address = '', city = '', country = '';
+        try {
+          const geo = await fetch('https://ipapi.co/json/').then(r => r.json());
+          ip_address = geo.ip || '';
+          city = geo.city || '';
+          country = geo.country_name || '';
+        } catch (_) {}
+
         await base44.entities.PageVisit.create({
           page: location.pathname,
           visitor_email,
           user_agent: navigator.userAgent,
           referrer: document.referrer || 'direct',
-          device: getDevice()
+          device: getDevice(),
+          device_name: getDeviceName(),
+          ip_address,
+          domain: window.location.hostname,
+          city,
+          country
         });
       } catch (_) {}
     };
