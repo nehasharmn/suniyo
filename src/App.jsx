9345bf6a7de1998235454a807a58e8ffcc0a795d
Pradeep Sharma <psharma@chdinvestments.net>
2026-03-24 18:09:05 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import AdminAnalytics from './pages/AdminAnalytics';
+import ChecklistBuilder from './pages/ChecklistBuilder';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
